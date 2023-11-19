50bc81552455d72262db94b25d55d093dea8a206
Martin Masevski <martin.masevski@gmail.com>
2023-11-19 18:05:14 +0100

    fix: scroll not needed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <TheHeader class="top-0 fixed" />
-  <div class="h-screen my-12 p-4">
+  <TheHeader class="top-0 sticky z-10" />
+  <div class="min-h-[calc(100vh_-_96px)] p-4 z-0">
     <slot />
   </div>
-  <TheFooter class="bottom-0 fixed" />
+  <TheFooter class="bottom-0 sticky z-10" />
 </template>
