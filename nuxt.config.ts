b57597a32304c3b0a9058b140c85c36151330e09
Martin Masevski <martin.masevski@gmail.com>
2024-03-23 20:23:08 +0100

    Refactor Nuxt config modules and add Formkit config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,18 @@ export default defineNuxtConfig({
       title: "Martin's Nuxt Starter",
     },
   },
-
-  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss', '@vueuse/nuxt', 'nuxt-lodash', 'nuxt-headlessui'],
+  modules: [
+    '@formkit/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    'nuxt-lodash',
+    'nuxt-headlessui',
+  ],
   css: ['@/assets/scss/main.scss'],
-
   tailwindcss: {
     cssPath: '@/assets/scss/main.scss',
     viewer: false,
   },
-
   /**
    * Nuxt Lodash config
    * @see https://nuxt.com/modules/lodash
@@ -21,7 +24,6 @@ export default defineNuxtConfig({
     prefix: '_',
     upperAfterPrefix: false,
   },
-
   /**
    * Nuxt Headless UI config
    * @see https://nuxt.com/modules/headlessui
@@ -29,15 +31,13 @@ export default defineNuxtConfig({
   headlessui: {
     prefix: 'Headless',
   },
-
-  devtools: {
-    enabled: true,
-  },
-
+  /**
+   * Formkit config
+   * @see https://formkit.com
+   */
   formkit: {
     autoImport: true,
   },
-
   postcss: {
     plugins: {
       tailwindcss: {},
