d6b8a0f0cd2bd124c3e149e44ab5a95093eeb6bf
Martin Masevski <martin.masevski@gmail.com>
2024-03-24 12:22:30 +0100

    Update header and footer components in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <TheHeader class="top-0 sticky z-10" />
+  <ExampleHeader class="top-0 sticky z-10" />
   <div class="min-h-[calc(100vh_-_96px)] p-4 z-0">
     <slot />
   </div>
-  <TheFooter class="bottom-0 sticky z-10" />
+  <ExampleFooter class="bottom-0 sticky z-10" />
 </template>
