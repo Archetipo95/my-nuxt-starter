58bb9f4774c16f2e4a1dab44974499dde8a2d0af
Martin Masevski <martin.masevski@gmail.com>
2025-06-30 15:21:33 +0200

    chore: add LICENSE file and update ESLint configuration to ignore it

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -47,7 +47,7 @@ export default withNuxt(antfu(
           kebabCase: true,
           camelCase: true,
         },
-        ignore: ["README.md", "FUNDING.yml"],
+        ignore: ["README.md", "FUNDING.yml", "LICENSE"],
       }],
       "object-curly-newline": ["error", {
         ObjectExpression: {
